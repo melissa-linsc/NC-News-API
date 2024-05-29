9416c4cce4c4745cd971af404008a4d0d6a0e006
melissa-linsc <118235468+melissa-linsc@users.noreply.github.com>
2024-05-29 17:08:35 +0200

    task 13: adds listen.js, .env.production and changes to package.json and connection.js

--- db/connection.js
+++ db/connection.js
@@ -5,10 +5,17 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-const connection = new Pool();
+const config = {};
+
+if (ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+const connection = new Pool(config);
 
 module.exports = connection

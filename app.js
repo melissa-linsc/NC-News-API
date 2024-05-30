f713e33226684e0ceef37d37eb3ab012b9963254
melissa-linsc <118235468+melissa-linsc@users.noreply.github.com>
2024-05-30 12:13:58 +0200

    changes test for length of endpoints.json from router refactor

--- app.js
+++ app.js
@@ -32,16 +32,4 @@ app.use((err, req, res, next) => {
     res.status(404).send({msg: 'Not Found'})
 })
 
-const countEndpoints = () => {
-    let count = 0
-    app._router.stack.forEach((middleware) => {
-        if (middleware.route) {
-            count ++
-        }
-    })
-    return count
-}
-
-countEndpoints()
-
-module.exports = {app, countEndpoints}
+module.exports = {app}

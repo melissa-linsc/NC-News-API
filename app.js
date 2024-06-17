f7d729defc34b7d49e3a7414c31b34ea590ea963
melissa-linsc <118235468+melissa-linsc@users.noreply.github.com>
2024-06-17 10:20:34 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ const getUsers = require('./controllers/users-controller')
 
 const apiRouter = require('./routes/api-router');
 
+const cors = require('cors');
+
 const app = express()
+
+app.use(cors());
 
 app.use(express.json())
 
